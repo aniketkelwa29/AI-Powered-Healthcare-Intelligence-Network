d2e522401739e59c84dad1c0bc537b2ea3e80b24
Capedcrusaders <150728323+Capedcrusaders@users.noreply.github.com>
2025-03-04 22:27:37 +0530

    Initial commit base code

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI-Powered Healthcare Intelligence Network",
+    page_icon="👋",
+)
+
+st.write("# AI-Powered Healthcare Intelligence Network 👋")
+
+st.sidebar.success("Select a demo above.")
